7efe0536e250d5a5128760475f81cf1dfbe60a6f
Carlos <ccanada05@gmail.com>
2024-10-24 18:36:25 +0200

    Hemos metido la navegación de menús con el bucle while y nos hemos atrancado en el manejo de fechas. Prox por hacer: Gestión de entradas y lo que surja.

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.time.*;
 import java.util.Scanner;
 
 /*Plantear dejar solo una variable de opciones. Habría que dejar solo un tipo de datos para las op pero nos ahorramos
@@ -8,14 +11,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-
-        /*Hay que crear variables de asientos para cada evento*/
-        /*Pensar en poner esperas entre pantallas (UX)*/
+        /*TODO Pensar en poner esperas entre pantallas (UX)*/
         final String userAdmin = "admin", passwdAdmin = "1234";
-        String user, passwd, option1;
-        int mainFloor = 200, armChair = 200, amphi = 200, option2 = 1, opEvent; //He puesto las variables que quedaban
-        // en inglés
+        String user, passwd, option1, opEvent = "";
+        /*El número al final de cada variable hace referencia a cada evento por orden de aparición*/
+        int mainFloor1 = 200, armChair1 = 200, amphi1 = 200, totalChair1 = 600,
+                mainFloor2 = 200, armChair2 = 200, amphi2 = 200, totalChair2 = 600,
+                mainFloor3 = 200, armChair3 = 200, amphi3 = 200, totalChair3 = 600, option2 = 1;
         float totalPrice;
+        LocalDate dateEntry;
+        SimpleDateFormat dateForm = new SimpleDateFormat("dd/MM/yyyy");
 
 
         System.out.println("""
@@ -27,81 +32,143 @@ public class Main {
                 ██║ ╚═╝ ██║███████╗ ╚████╔╝ ███████╗██║ ╚████║   ██║   ███████║
                 ╚═╝     ╚═╝╚══════╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝
                 """);
-        do { /*
-        He empezado a meter el bucle pero hay que averiguar cual debe ser la condición de salida para
-        cerrar los menús
-        */
-            System.out.println("""
+        while (option2 != 3) { /*Con el bucle while, mientras la opcion2 no sea 3 (opción de salida en el menú de admin
+        permite apagar el programa*/
+            System.out.println(""" 
                                    
-                    ╔════════════════════════════════════════════╗
-                    ║  ********     MENU PRINCIPAL     ********  ║
-                    ╠════════════════════════════════════════════╣
-                    ║  a. Vender entradas para un evento.        ║
-                    ║  b. Consultar el estado de un evento.      ║
-                    ║  c. Menú de Administrador                  ║
-                    ╚════════════════════════════════════════════╝""");
-            System.out.print("selecciona una opción: ");
+                    \t╔════════════════════════════════════════════╗
+                    \t║  ********     MENU PRINCIPAL     ********  ║
+                    \t╠════════════════════════════════════════════╣
+                    \t║  a. Vender entradas para un evento.        ║
+                    \t║  b. Consultar el estado de un evento.      ║
+                    \t║  c. Menú de Administrador                  ║
+                    \t╚════════════════════════════════════════════╝""");//Le metemos un tabulador al principio para
+            //centrar el display de los menús y opciones
+            System.out.print("\tSelecciona una opción: ");
             option1 = s.nextLine();
             option1 = option1.toLowerCase();
-            /*NOS HEMOS QUEDADO AQUI 21/10/*/
             switch (option1) {
                 case "a":
-                    System.out.println("""
-                                           
-                            ╔═══════════════════════════════════════════╗
-                            ║        ********   EVENTO   ********       ║
-                            ╠═══════════════════════════════════════════╣
-                            ║  1. Las Criadas.                          ║
-                            ║  2. II Concierto de Otoño.                ║
-                            ║  3. Concierto de Jazz.                    ║
-                            ╚═══════════════════════════════════════════╝""");
-                    System.out.print("Introduce un evento: ");
-                    opEvent = Integer.parseInt(s.nextLine());
-                    switch (opEvent) {
-                        /*
-                         * He incluido la info de cada concierto según la opción introducida. Habría que
-                         * implementar la opción de compra y cuántas entradas quiere. Creo que podría ser otro switch
-                         * que le preguntase si quiere comprar o no (yendo así al menú anterior), y si es que si preguntar
-                         * cuantas quiere y hacer la lógica al respecto
-                         * */
-                        case 1:
-                            System.out.println("""
-                                    ╔═══════════════════════════════════════════╗
-                                    ║     ********   Las Criadas   ********     ║
-                                    ╠═══════════════════════════════════════════╣
-                                    ║  - Fecha: 20/11/2024                      ║
-                                    ║  - Precio Platea: 17€                     ║
-                                    ║  - Precio Butaca: 20€                     ║
-                                    ║  - Precio Anfiteatro: 13€                 ║
-                                    ║  - Desc. Anticipado: 5%                   ║
-                                    ╚═══════════════════════════════════════════╝""");
-                            break;
-                        case 2:
-                            System.out.println("""
-                                    ╔═════════════════════════════════════════════════════╗
-                                    ║     ********   II Concierto de Otoño   ********     ║
-                                    ╠═════════════════════════════════════════════════════╣
-                                    ║  - Fecha: 28/11/2024                                ║
-                                    ║  - Precio Platea: 12€                               ║
-                                    ║  - Precio Butaca: 15€                               ║
-                                    ║  - Precio Anfiteatro: 8€                            ║
-                                    ║  - Desc. Anticipado: 7%                             ║
-                                    ╚═════════════════════════════════════════════════════╝""");
-                            break;
-                        case 3:
-                            System.out.println("""
-                                    ╔═════════════════════════════════════════════════╗
-                                    ║     ********   Concierto de Jazz   ********     ║
-                                    ╠═════════════════════════════════════════════════╣
-                                    ║  - Fecha: 06/12/2024                            ║
-                                    ║  - Precio Platea: 20€                           ║
-                                    ║  - Precio Butaca: 25€                           ║
-                                    ║  - Precio Anfiteatro: 15€                       ║
-                                    ║  - Desc. Anticipado: 3%                         ║
-                                    ╚═════════════════════════════════════════════════╝""");
-                            break;
-                        default:
-                            System.out.println("opción no valida");
+                    while (!option1.equalsIgnoreCase("4")) {
+                        System.out.println("""
+                                               
+                                \t╔═══════════════════════════════════════════╗
+                                \t║        ********   EVENTO   ********       ║
+                                \t╠═══════════════════════════════════════════╣
+                                \t║  1. Las Criadas.                          ║
+                                \t║  2. II Concierto de Otoño.                ║
+                                \t║  3. Concierto de Jazz.                    ║
+                                \t   4. Menú principal
+                                \t╚═══════════════════════════════════════════╝""");
+                        System.out.print("\tIntroduce un evento: ");
+                        option1 = s.nextLine();
+                        switch (option1) {
+                            /*
+                             * He incluido la info de cada concierto según la opción introducida. Habría que
+                             * implementar la opción de compra y cuántas entradas quiere. Creo que podría ser otro switch
+                             * que le preguntase si quiere comprar o no (yendo así al menú anterior), y si es que si preguntar
+                             * cuantas quiere y hacer la lógica al respecto
+                             * */
+                            case "1":
+                                while (!opEvent.equalsIgnoreCase("2")) {
+                                    System.out.println("""
+                                            \t╔═══════════════════════════════════════════╗
+                                            \t║     ********   Las Criadas   ********     ║
+                                            \t╠═══════════════════════════════════════════╣
+                                            \t║  - Fecha: 20/11/2024                      ║
+                                            \t║  - Precio Platea: 17€                     ║
+                                            \t║  - Precio Butaca: 20€                     ║
+                                            \t║  - Precio Anfiteatro: 13€                 ║
+                                            \t║  - Desc. Anticipado: 5%                   ║
+                                            \t║                                           ║
+                                            \t║    1. Compra                 2. Menú      ║
+                                            \t╚═══════════════════════════════════════════╝""");
+
+
+                                    System.out.print("\tSeleccione una opción: ");
+                                    opEvent = s.nextLine();
+                                    switch (opEvent) {
+                                        case "1":
+                                            /*TODO NOS HEMOS QUEDADO POR AQUÍ 24/10/2024, INVESTIGAR FECHA*/
+                                            System.out.print("Introduzca la fecha ('no' para coger fecha actual): ");
+                                            dateEntry = LocalDate.parse(s.nextLine());
+                                            System.out.println(dateForm.format(dateEntry));
+                                            break;
+                                        case "2":
+                                            System.out.println("Volver");
+                                            break;
+                                        default:
+                                            System.out.println("Opción no válida");
+                                            break;
+                                    }
+                                }
+                                break;
+                            case "2":
+                                while (!opEvent.equalsIgnoreCase("2")) {
+                                    System.out.println("""
+                                            \t╔═════════════════════════════════════════════════════╗
+                                            \t║     ********   II Concierto de Otoño   ********     ║
+                                            \t╠═════════════════════════════════════════════════════╣
+                                            \t║  - Fecha: 28/11/2024                                ║
+                                            \t║  - Precio Platea: 12€                               ║
+                                            \t║  - Precio Butaca: 15€                               ║
+                                            \t║  - Precio Anfiteatro: 8€                            ║
+                                            \t║  - Desc. Anticipado: 7%                             ║
+                                            \t║                                                     ║
+                                            \t║    1. Compra                 2. Menú                ║
+                                            \t╚═════════════════════════════════════════════════════╝""");
+
+
+                                    System.out.print("\tSeleccione una opción: ");
+                                    opEvent = s.nextLine();
+                                    switch (opEvent) {
+                                        case "1":
+                                            System.out.println("Compra");
+                                            break;
+                                        case "2":
+                                            System.out.println("Volver");
+                                            break;
+                                        default:
+                                            System.out.println("Opción no válida");
+                                            break;
+                                    }
+                                }
+                                break;
+                            case "3":
+                                while (!opEvent.equalsIgnoreCase("2")) {
+                                    System.out.println("""
+                                            ╔═════════════════════════════════════════════════╗
+                                            ║     ********   Concierto de Jazz   ********     ║
+                                            ╠═════════════════════════════════════════════════╣
+                                            ║  - Fecha: 06/12/2024                            ║
+                                            ║  - Precio Platea: 20€                           ║
+                                            ║  - Precio Butaca: 25€                           ║
+                                            ║  - Precio Anfiteatro: 15€                       ║
+                                            ║  - Desc. Anticipado: 3%                         ║
+                                            \t║                                               ║
+                                            \t║    1. Compra                 2. Menú          ║
+                                            ╚═════════════════════════════════════════════════╝""");
+
+                                    System.out.print("\tSeleccione una opción: ");
+                                    opEvent = s.nextLine();
+                                    switch (opEvent) {
+                                        case "1":
+                                            System.out.println("Compra");
+                                            break;
+                                        case "2":
+                                            System.out.println("Volver");
+                                            break;
+                                        default:
+                                            System.out.println("Opción no válida");
+                                            break;
+                                    }
+                                }
+                                break;
+                            case "4":
+                                break;
+                            default:
+                                System.out.println("opción no valida");
+                        }
                     }
                     break;
                 case "b":
@@ -163,11 +230,6 @@ public class Main {
                 default:
                     System.out.println("El valor introducido no es correcto es incorrecto.");
             }
-        } while (option2 != 3);/*"E:"Se estaba repitiendo ya que estaba haciendo la lógica de se repita mientras opción
-         sera igual a 3 ahora al ser distinto de 3 se sale :).
-        "C:"Este while hace referencia al primer do, en el que está metido el primer menú
-        no funciona, repite el bucle una y otra vez pero hay que investigar qué condición es la que rompe todo el
-        bucle
-        */
+        }
     }
 }
